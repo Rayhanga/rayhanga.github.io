a17d873a06ab0d67f609b1150a7278a488424294
Rayhan Gusty Alif <vhawk23@gmail.com>
2018-01-21 15:14:03 +0700

    Create fresh site

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Index extends React.Component {
+  componentDidMount(){
+    document.title ="Rayhan Gusty Alif";
+  }
+  render() {
+    return (
+      <div>
+      <h1>Rayhan Gusty Alif</h1>
+      <p>A simple website made with react.js</p>
+      </div>
+    );
+  };
+};
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+);

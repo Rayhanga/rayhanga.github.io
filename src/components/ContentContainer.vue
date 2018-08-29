f062fcf1e6f3285259d528f4a1fa35bbab1c4b17
Rayhan Gusty Alif <vhawk23@gmail.com>
2018-08-29 13:01:55 +0700

    Add content container components

---
+++ src/components/ContentContainer.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-container fluid>
+		<v-layout row wrap>
+		<ContentItem :key="content.id" v-for="content in contents" :content="content"/>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+import ContentItem from "./ContentItem.vue"
+import contData from "../data/ctn.json"
+
+export default {
+	name: 'ContentContainer',
+	components: {
+		ContentItem
+	},
+	data () {
+		return {
+			contents: contData
+		}
+	}
+}
+</script>
---
+++ src/components/ContentItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-flex xs6 pa-2>
+      <v-card py-2 class="ContentItem">
+        <v-card-title primary-title>
+          <h1>{{ content.title }}</h1>
+        </v-card-title>
+        <v-card-text>
+          <p>
+            {{ content.text}}
+          </p>
+        </v-card-text>
+      </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'ContentItem',
+  props: {
+    content: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>

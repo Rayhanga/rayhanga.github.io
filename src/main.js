4e2e57acc68029dae967e27206da4807104aa939
Rayhan Gusty Alif <vhawk23@gmail.com>
2018-08-30 17:13:53 +0700

    Install Firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import VueRouter from 'vue-router';
+import VueRouter from 'vue-router'
+import firebase from 'firebase'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'material-icons/iconfont/material-icons.css';
 import 'vuetify/dist/vuetify.min.css'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBRklyYIHs9q3g-XICl8_lZ7gXlBxK3t8U",
+  authDomain: "porto-web-data.firebaseapp.com",
+  databaseURL: "https://porto-web-data.firebaseio.com",
+  projectId: "porto-web-data",
+  storageBucket: "porto-web-data.appspot.com",
+  messagingSenderId: "288626834727"
+};
+firebase.initializeApp(config);
 
 export const serverBus = new Vue();
 
